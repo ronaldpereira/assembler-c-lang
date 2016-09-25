12682863d50346fdc1bf1a07bdce788d151b38e6
ronaldpereira <ronald.drp11@gmail.com>
2016-09-25 13:08:08 -0300

    Correção do Makefile, impressão de headers do output.mif e correção de impressões do Makefile

--- code/assembler/assembler.c
+++ code/assembler/assembler.c
@@ -5,7 +5,21 @@
 
 int main(int argc, char* argv[])
 {
-	
+	FILE *input, *output;
+
+	input = fopen(argv[1], "r");
+	output = fopen(argv[2], "w+r");
+
+	// Input fscanf
+
+	//Output fprint
+	fprintf(output, "DEPTH = 256;\nWIDTH = 8;\nADDRESS_RADIX = BIN;\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n\n");
+
+	//Assembly impression
+
+	fprintf(output, "END;\n");
+
+	fprintf(stdout, "\n***Program Outputs***\nFile '%s' assembled sucessfully into '%s'.\n", argv[1], argv[2]);
 
 	return 0;
 }
