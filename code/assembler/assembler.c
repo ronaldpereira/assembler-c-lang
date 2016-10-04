dce9a7de3b4447a4f6c17b98be90a28f61a9dc06
ronaldpereira <ronald.drp11@gmail.com>
2016-10-04 18:10:56 -0300

    Inclusão do loadi e das leituras mais simples

--- code/assembler/assembler.c
+++ code/assembler/assembler.c
@@ -16,11 +16,34 @@ bool *binaryConversion(bool *bin, int dec)
 	return bin;
 }
 
+bool *binaryTwoComplement(bool *comp, int dec)
+{
+	int i, flag = 0;
+
+	binaryConversion(comp, dec);
+
+	for(i = 7; i >= 0; i--)
+	{
+		if(comp[i] == 1 && flag == 0)
+			flag = 1;
+
+		else if(flag == 1)
+		{
+			if(comp[i] == 0)
+				comp[i] = 1;
+			else
+				comp[i] = 0;
+		}
+	}
+
+	return comp;
+}
+
 int main(int argc, char* argv[])
 {
 	FILE *input, *output;
 	bool *binary;
-	int i, j, flag;
+	int pc, j, flag, dec;
 	char *line, *token;
 
 	input = fopen(argv[1], "r");
@@ -32,15 +55,14 @@ int main(int argc, char* argv[])
 
 	fprintf(output, "DEPTH = 256;\nWIDTH = 8;\nADDRESS_RADIX = BIN;\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n\n");
 
-	i = 0;
-	while(i < 256)
+	pc = 0;
+	while(pc < 256)
 	{
-		binaryConversion(binary, i);
+		binaryConversion(binary, pc);
 		for(j = 0; j < 8; j++)
 			fprintf(output, "%d", binary[j]);
-		fprintf(output, "  :  \n");
+		fprintf(output, "  :  ");
 
-		// Aqui vão ficar as instruções de leitura e tradução do input
 		flag = 0;
 		while(fscanf(input, "%[^\n]\n", line) != EOF)
 		{
@@ -55,30 +77,87 @@ int main(int argc, char* argv[])
 		{
 			token = strtok(line, " \t");
 
-			if(token[0] == '_')
+			if(token[0] == '_') // Identifica um Label
 			{
-				//Salva o label em uma struct
+				//Salva o label em uma lista de label
 				token = strtok(token, ":");
 				printf("Struct = %s\n", token);
 			}
+
+			/*else if(token[strlen(token)] == ':') // Identifica uma pseudoinstrução .data
+			{
+				//Salva o .data em um lista value
+				token = strtok(NULL, " \t");
+				if(strcmp(token, ".data") == 0)
+				{
+					token = strtok(NULL, " \t");
+					for(j = 0; j < atoi(token); j++)
+					{
+
+						pc++;
+					}
+				}
+			}*/
 
 			else
 			{
 				//Identifica uma instrução
 				printf("Instrução = %s\n", token);
-				//Switch case para cada instrução
-				
+				//If-elseif para cada instrução
+
 				if(strcmp(token, "exit") == 0)
 				{
 					fprintf(output, "00000000\n");
-					i++;
-					binaryConversion(binary, i);
+					pc++;
+					binaryConversion(binary, pc);
 					for(j = 0; j < 8; j++)
 						fprintf(output, "%d", binary[j]);
 					fprintf(output, "  :  ");
 					fprintf(output, "00000000\n");
 				}
-				/*
+
+				else if(strcmp(token, "loadi") == 0)
+				{
+					fprintf(output, "00001");
+					token = strtok(NULL, " \t");
+					if(token[1] >= '0' && token[1] <= '8') // É um registrador válido (R0 até R8)
+					{
+						dec = (int)(token[1] - '0'); // Transformação de string para inteiro
+						printf("%d\n", dec);
+						binaryConversion(binary, dec);
+						for(j = 5; j < 8; j++)
+						{
+							fprintf(output, "%d", binary[j]);
+						}
+						fprintf(output, ";\n");
+						pc++;
+						binaryConversion(binary, pc);
+						for(j = 0; j < 8; j++)
+							fprintf(output, "%d", binary[j]);
+						fprintf(output, "  :  ");
+					}
+
+					token = strtok(NULL, " \t");
+					if(strcmp(token, "IO") == 0)
+						fprintf(output, "11111110;\n");
+
+					else if(token[0] >= '0' && token[0] <= '9') // É um imediato positivo válido
+					{
+						dec = (int)(token[1] - '0'); // Transformação de string para inteiro
+						binaryConversion(binary, dec);
+						for(j = 5; j < 8; j++)
+						{
+							fprintf(output, "%d", binary[j]);
+						}
+						fprintf(output, ";\n");
+					}
+
+					else // É qualquer outro tipo de dado (pseudoinstrução .data)
+					{
+
+					}
+				}
+
 				else if(strcmp(token, "") == 0)
 				{
 
@@ -86,58 +165,52 @@ int main(int argc, char* argv[])
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
+
 				}
 
 				else if(strcmp(token, "") == 0)
 				{
-					
-				}
 
-				else if(strcmp(token, "") == 0)
-				{
-					
 				}
-				*/
 			}
 		}
 
-		i++;
+		pc++;
 	}
 
 	fprintf(output, "END;\n");
